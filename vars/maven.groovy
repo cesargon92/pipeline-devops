b9a4fcc16c05330b231e522d733cb6436c9ce6a5
César González Cornejo <cesar.gonzalez.c@wom.cl>
2021-01-05 20:38:21 -0300

    se completa script maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,26 +1,69 @@
 def call(List<String> arrStages) {
     println("Cantidad de Stages ingresados: " + arrStages.size())
-    stage('Compile') {
-        env.TASK = env.STAGE_NAME
-        sh 'mvn clean compile -e'
+
+    boolean buildOk = false;
+
+    if (arrStages.size() == 0){
+        arrStages = ["Compile", "Test", "Package", "SonarQube", "UploadNexus"]
     }
-    stage('Test') {
-        env.TASK = env.STAGE_NAME
-        sh 'mvn clean test -e'
-    }
-    stage('Package') {
-        env.TASK = env.STAGE_NAME
-        sh 'mvn clean package -e'
-    }
-    stage('SonarQube') {
-        env.TASK = env.STAGE_NAME
-        withSonarQubeEnv(installationName: 'sonar-server') {
-            sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+
+    for (String str in arrStages){
+        if (str.equalsIgnoreCase("Compile")){
+            stage('Compile') {
+                env.TASK = env.STAGE_NAME
+                sh 'mvn clean compile -e'
+            }
+            break;
         }
     }
-    stage('UploadNexus') {
-        env.TASK = env.STAGE_NAME
-        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:/Proyectos/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+    
+    for (String str in arrStages){
+        if (str.equalsIgnoreCase("Test")){
+            stage('Test') {
+                env.TASK = env.STAGE_NAME
+                sh 'mvn clean test -e'
+            }
+            break;
+        }
+    }
+    
+    for (String str in arrStages){
+        if (str.equalsIgnoreCase("Build")){
+            stage('Package') {
+                env.TASK = env.STAGE_NAME
+                sh 'mvn clean package -e'
+            }
+            buildOk = true;
+            break;
+        }
+    }
+
+    for (String str in arrStages){
+        if (str.equalsIgnoreCase("SonarQube")){
+            if (buildOk){
+                stage('SonarQube') {
+                    env.TASK = env.STAGE_NAME
+                    withSonarQubeEnv(installationName: 'sonar-server') {
+                        sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+                    }
+                }
+            } else {
+                throw new Exception("Deteniendo pipeline. No se ha contruido .jar para ejecutar esta tarea")
+            }
+        }
+    }
+
+    for (String str in arrStages){
+        if (str.equalsIgnoreCase("UploadNexus")){
+            if (buildOk){
+                stage('UploadNexus') {
+                    env.TASK = env.STAGE_NAME
+                    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:/Proyectos/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+                }
+            } else {
+                throw new Exception("Deteniendo pipeline. No se ha contruido .jar para ejecutar esta tarea")
+            }
+        }
     }
 }
 
